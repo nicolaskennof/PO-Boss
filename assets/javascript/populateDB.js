ef3dfa80e375df27b4b1155110dfbe36e5bdb719
Seiji Osada <seiji13r@outlook.com>
2018-10-06 12:33:41 -0500

    Readme updated

--- assets/javascript/populateDB.js
+++ assets/javascript/populateDB.js
@@ -7,20 +7,23 @@ let populateDB = function(){
         {
             poId: "1234",
             zone: "A",
+            dateAdded: firebase.database.ServerValue.TIMESTAMP,
         },
         {
             poId: "456",
             zone: "B",
+            dateAdded: firebase.database.ServerValue.TIMESTAMP,
         },
         {
             poId: "2187941",
             zone: "C",
+            dateAdded: firebase.database.ServerValue.TIMESTAMP,
         },
     ]
 
     for(var i = 0; i < dbRecords.length; i++){
         console.log(dbRecords[i]);
-        // database.ref().push(dbRecords[i]);
+        database.ref().push(dbRecords[i]);
     }
 
 };
