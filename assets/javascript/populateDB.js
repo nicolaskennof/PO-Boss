baadec3ec9d1690e8b9925c09431af6f9a7a4da9
Seiji Osada <seiji13r@outlook.com>
2018-10-06 14:48:52 -0500

    Update populateDB()

--- assets/javascript/populateDB.js
+++ assets/javascript/populateDB.js
@@ -1,22 +1,51 @@
 let populateDB = function(){
+    
+    database.ref().remove();
+
     dbRecords = [
         // {
         //     poId: "",
         //     zone: "",
+        //     company: "",
+        //     street: "",
+        //     colony: "",
+        //     zone: "", // A, B, C ,D ,E.
+        //     lon: "",
+        //     lat: "",
+        //     poDateTime: "",
+        //     products: "",
+        //     totalPrice: "",
+        //     status: "", // ontrack, delivered, delayed, incomplete.
+        //     dateAdded: firebase.database.ServerValue.TIMESTAMP,
         // },
         {
-            poId: "1234",
+            poId: "12345",
             zone: "A",
+            company: "Constructura X",
+            street: "constituyentes",
+            colony: "Pueblito",
+            zone: "A", // A, B, C ,D ,E.
+            lon: "-100.45178387",
+            lat: "20.55877237",
+            poDateTime: "",
+            products: "",
+            totalPrice: "",
+            status: "", // ontrack, delivered, delayed, incomplete.
             dateAdded: firebase.database.ServerValue.TIMESTAMP,
         },
         {
-            poId: "456",
-            zone: "B",
-            dateAdded: firebase.database.ServerValue.TIMESTAMP,
-        },
-        {
-            poId: "2187941",
-            zone: "C",
+            poId: "12345",
+            zone: "",
+            company: "",
+            street: "Benito Juarez",
+            colony: "ZIBATA",
+            zone: "B", // A, B, C ,D ,E.
+            lon: "-100.3331444",
+            lat: "20.68051591",
+            poDateTime: "",
+            products: "",
+            totalPrice: "200",
+            status: "", // ontrack, delivered, delayed, incomplete.
             dateAdded: firebase.database.ServerValue.TIMESTAMP,
         },
     ]
