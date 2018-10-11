a79b411a6a347d9a364c3cfa8914a7f53927c42f
ana gonzález <ana@MacBook-de-ana.local>
2018-10-10 20:42:06 -0500

    cambios en draw Table

--- assets/javascript/drawTable.js
+++ assets/javascript/drawTable.js
@@ -1,4 +1,62 @@
+ // set endpoint and your access key
+ //var totalPrice=0; //aquí es poner que sea el número que sale en la tabla con el precio en MXN
+ function convertToUSD(totalPrice){
+    var endpoint = 'live';
+    var access_key = 'e208a825539a0db2517710f6457203b5';
+    var result;
+    // get the most recent exchange rates via the "live" endpoint:
+    $.ajax({
+        url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,
+        success: function (json) {
 
+            // exchange rata data is stored in json.quotes
+            //console.log(json.quotes.USDMXN);
+            var USDMXN=json.quotes.USDMXN;
+            console.log("tipo de cambio "+USDMXN);
+
+            // source currency is stored in json.source
+            ///console.log(json.source);
+            var totalPriceUSD=Math.round(totalPrice/USDMXN*100)/100;
+            console.log(totalPriceUSD);
+            result=totalPriceUSD;
+        }
+    });
+    console.log(result);
+    return result;
+};
+ //countdown
+ var deadline = 'october 31 2018'; //cambiar a poDateTime+48h
+ function getTimeRemaining(endtime){
+      var t = Date.parse(endtime) - Date.parse(new Date());
+     var seconds = Math.floor( (t/1000) % 60 );
+     var minutes = Math.floor( (t/1000/60) % 60 );
+     var hours = Math.floor( (t/(1000*60*60)) % 24 );
+     var days = Math.floor( t/(1000*60*60*24) );
+         return {
+             'total': t,
+             'days': days,
+             'hours': hours,
+             'minutes': minutes,
+             'seconds': seconds
+         };
+}
+
+ function initializeClock(id, endtime){
+ var clock = document.getElementById(id);
+ var timeinterval = setInterval(function(){
+     var t = getTimeRemaining(endtime);
+     clock.innerHTML = 'days: ' + t.days + '<br>' +
+                     'hours: '+ t.hours + '<br>' +
+                     'minutes: ' + t.minutes + '<br>';
+     if(t.total<=0){
+     clearInterval(timeinterval);
+     }
+ },1000);
+ }
+
+ //initializeClock('clockdiv', deadline);
+
+ 
 // Firebase watcher .on("child_added"
 database.ref().on("child_added", function(snapshot) {
 
@@ -27,6 +85,7 @@ database.ref().on("child_added", function(snapshot) {
 
     let priceUsdTd = $("<td>");
     // Convert MXN to USD
+    priceUsdTd.text(convertToUSD(snapshot.val().totalPrice));
 
     let timeLeftTd = $("<td>");
     // Calculate it with moment JS
@@ -48,3 +107,8 @@ database.ref().on("child_added", function(snapshot) {
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
+
+
+/*Acomodar AJAX
+llamar bien el tiempo remaining
+Unix más 48*/ 
