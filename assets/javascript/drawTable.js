5dbf5f14b7b83d7004963b91190d7986fe0917f1
ana gonzález <ana@MacBook-de-ana.local>
2018-10-12 11:19:45 -0500

    drawtable

--- assets/javascript/drawTable.js
+++ assets/javascript/drawTable.js
@@ -1,37 +1,35 @@
- // set endpoint and your access key
- //var totalPrice=0; //aquí es poner que sea el número que sale en la tabla con el precio en MXN
- function convertToUSD(totalPrice){
-    var endpoint = 'live';
-    var access_key = 'e208a825539a0db2517710f6457203b5';
-    var result;
-    // get the most recent exchange rates via the "live" endpoint:
+var currencyRateCache=null;
+//funcion para convertir MXN a USD
+ function convertToUSD(callbackPrice){
+    
+    if (currencyRateCache!==null){
+        callbackPrice(currencyRateCache);   
+    };
+
     $.ajax({
-        url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,
-        success: function (json) {
-
-            // exchange rata data is stored in json.quotes
-            //console.log(json.quotes.USDMXN);
-            var USDMXN=json.quotes.USDMXN;
-            console.log("tipo de cambio "+USDMXN);
-
-            // source currency is stored in json.source
-            ///console.log(json.source);
-            var totalPriceUSD=Math.round(totalPrice/USDMXN*100)/100;
-            console.log(totalPriceUSD);
-            result=totalPriceUSD;
-        }
+        url: 'https://free.currencyconverterapi.com/api/v6/convert?q=USD_MXN,MXN_USD&compact=ultra',
+        method: "GET"
+    }).then(function(json){
+        //console.log(json);
+        currencyRateCache=json.USD_MXN;
+        callbackPrice(currencyRateCache);
+        console.log("cambio"+currencyRateCache);
+    }).fail(function(error){
+        callbackPrice(19);
     });
-    console.log(result);
-    return result;
+   
 };
- //countdown
- var deadline = 'october 31 2018'; //cambiar a poDateTime+48h
+
+
+ //difference between current time and deadline
  function getTimeRemaining(endtime){
-      var t = Date.parse(endtime) - Date.parse(new Date());
-     var seconds = Math.floor( (t/1000) % 60 );
-     var minutes = Math.floor( (t/1000/60) % 60 );
-     var hours = Math.floor( (t/(1000*60*60)) % 24 );
-     var days = Math.floor( t/(1000*60*60*24) );
+    var currentTime=Math.round((new Date()).getTime() / 1000);
+    var deadline= parseInt(endtime)+48*60*60;
+    var t = deadline - currentTime;
+     var seconds = Math.floor( (t) % 60 );
+     var minutes = Math.floor( (t/60) % 60 );
+     var hours = Math.floor( (t/(60*60)) % 24 );
+     var days = Math.floor( t/(60*60*24));
          return {
              'total': t,
              'days': days,
@@ -40,75 +38,77 @@
              'seconds': seconds
          };
 }
-
- function initializeClock(id, endtime){
- var clock = document.getElementById(id);
- var timeinterval = setInterval(function(){
-     var t = getTimeRemaining(endtime);
-     clock.innerHTML = 'days: ' + t.days + '<br>' +
-                     'hours: '+ t.hours + '<br>' +
-                     'minutes: ' + t.minutes + '<br>';
-     if(t.total<=0){
-     clearInterval(timeinterval);
-     }
- },1000);
- }
-
- //initializeClock('clockdiv', deadline);
+//show the time remaining
+ function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var timeinterval = setInterval(function(){
+        var t = getTimeRemaining(endtime);
+        clock.innerHTML = 'days: ' + t.days + '<br>' +
+                        'hours: '+ t.hours + '<br>';
+        if(t.total<=0){
+        clearInterval(timeinterval);
+        }
+    },1000);
+}
 
  
-// Firebase watcher .on("child_added"
-database.ref().on("child_added", function(snapshot) {
 
-    // <th>PO Id</th>
-    // <th>Customer</th>
-    // <th>Zone</th>
-    // <th>Total Price [MXN]</th>
-    // <th>Total Price [USD]</th>
-    // <th>Time Left Before Delivery</th>
-    // <th>Status</th>
+convertToUSD(function(USDMXN){ 
+    // Firebase watcher .on("child_added"
+    database.ref().on("child_added", function(snapshot) {
 
-    let row = $('<tr id="' + snapshot.key + '">');
-    row.addClass("row-class");
+        // <th>PO Id</th>
+        // <th>Customer</th>
+        // <th>Zone</th>
+        // <th>Total Price [MXN]</th>
+        // <th>Total Price [USD]</th>
+        // <th>Time Left Before Delivery</th>
+        // <th>Status</th>
 
-    let poIdTd = $("<td>");
-    poIdTd.text(snapshot.val().poId);
+            let row = $('<tr id="' + snapshot.key + '">');
+            row.addClass("row-class");
+        
+            let poIdTd = $("<td>");
+            poIdTd.text(snapshot.val().poId);
+        
+            let customerTd = $("<td>");
+            customerTd.text(snapshot.val().clientSite);
+        
+            let zoneTd = $("<td>");
+            zoneTd.text(snapshot.val().zone);
+        
+            let priceMxnTd = $("<td>");
+            priceMxnTd.text("$"+snapshot.val().totalPrice);
+        
+            let priceUsdTd = $("<td>");
+            // Convert MXN to USD
+            let priceUSD=Math.round((snapshot.val().totalPrice/USDMXN)*100)/100;
+            console.log(priceUSD);
+            priceUsdTd.text("$"+priceUSD);
+            
+            
+            let timeLeftTd = $("<td>");
+            //giving an id per timecell
+            timeLeftTd.attr("id","time"+snapshot.val().poId);
+        
+            let statusTd = $("<td>");
+            statusTd.text(snapshot.val().status);
+        
+            row.append(poIdTd);
+            row.append(customerTd);
+            row.append(zoneTd);
+            row.append(priceMxnTd);
+            row.append(priceUsdTd);
+            row.append(timeLeftTd);
+            row.append(statusTd);
+        
+            row.appendTo(poTable);
+            initializeClock("time"+snapshot.val().poId,snapshot.val().poDateTime);
+        
 
-    let customerTd = $("<td>");
-    customerTd.text(snapshot.val().clientSite);
-
-    let zoneTd = $("<td>");
-    zoneTd.text(snapshot.val().zone);
-
-    let priceMxnTd = $("<td>");
-    priceMxnTd.text(snapshot.val().totalPrice);
-
-    let priceUsdTd = $("<td>");
-    // Convert MXN to USD
-    priceUsdTd.text(convertToUSD(snapshot.val().totalPrice));
-
-    let timeLeftTd = $("<td>");
-    // Calculate it with moment JS
-
-    let statusTd = $("<td>");
-    statusTd.text(snapshot.val().status);
-
-    row.append(poIdTd);
-    row.append(customerTd);
-    row.append(zoneTd);
-    row.append(priceMxnTd);
-    row.append(priceUsdTd);
-    row.append(timeLeftTd);
-    row.append(statusTd);
-
-    row.appendTo(poTable);
-
-    // Handle the errors
-}, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
+        // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
 });
 
-
-/*Acomodar AJAX
-llamar bien el tiempo remaining
-Unix más 48*/ 
